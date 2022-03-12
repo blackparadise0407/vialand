1660d01530849d1556c6bc57b1d95e62ac05b2ec
blackparadise0407 <blackparadise0407@gmail.com>
2022-03-13 01:05:46 +0700

    Fix NewsCard component

--- src/components/NewsCard/index.tsx
+++ src/components/NewsCard/index.tsx
@@ -8,12 +8,16 @@ type NewsCardProps = {
 export default function NewsCard({ data }: NewsCardProps) {
   if (!data) return null
 
-  const { subject, image } = data
+  const { subject, images } = data
 
   return (
     <Link to={`/nha-dat/${data.id}`}>
       <div className="flex gap-5 hover:shadow p-2 cursor-pointer">
-        <img className="w-[128px]" src={image ?? second} alt="" />
+        <img
+          className="w-[128px] aspect-video"
+          src={images?.length ? images[0] : second}
+          alt=""
+        />
         <div className="overflow-hidden">
           <p className="truncate text-base font-light">{subject}</p>
         </div>
