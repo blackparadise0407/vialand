1ecc2d02df46fb311c77343fccffedd35319c007
blackparadise0407 <blackparadise0407@gmail.com>
2022-03-16 01:58:14 +0700

    Implement Result component

---
+++ src/components/Result/index.tsx
@@ -0,0 +1,11 @@
+type ResultProps = {
+  title: string
+}
+
+export default function Result({ title }: ResultProps) {
+  return (
+    <div className="flex py-4 px-2 flex-col items-center">
+      <span className="p-5 font-medium bg-gray-100 rounded">{title}</span>
+    </div>
+  )
+}
