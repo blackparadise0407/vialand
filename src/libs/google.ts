71fd052934244ebc35b51dfcdee94da431f87b8a
blackparadise0407 <blackparadise0407@gmail.com>
2022-03-23 01:04:30 +0700

    Add more functions

--- src/libs/google.ts
+++ src/libs/google.ts
@@ -1,10 +1,10 @@
 import { CallbackDoc } from 'react-google-drive-picker/dist/typeDefs'
 
-export const getFilesMetadata = async (files: CallbackDoc[], token: string) => {
+export const getFilesMetadata = (files: CallbackDoc[], token: string) => {
   return Promise.all(
     files.map((file) =>
       fetch(
-        `https://www.googleapis.com/drive/v2/files/${file.id}?fields=embedLink`,
+        `https://www.googleapis.com/drive/v2/files/${file.id}?fields=thumbnailLink,embedLink,id`,
         {
           method: 'GET',
           headers: {
@@ -16,8 +16,29 @@ export const getFilesMetadata = async (files: CallbackDoc[], token: string) => {
   )
     .then((responses) =>
       Promise.all(responses.map((response) => response.json())).then((data) =>
-        data.map((x) => x.embedLink as string),
+        data.map(
+          ({ id, thumbnailLink, embedLink }) =>
+            ({
+              id,
+              thumbnailLink,
+              embedLink,
+            } as IGoogleDocsResponse),
+        ),
       ),
     )
     .catch()
+}
+
+export const removeFileFromDriveById = (
+  fileId: string,
+  token: string,
+): Promise<boolean> => {
+  return fetch(`https://www.googleapis.com/drive/v3/files/${fileId}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then(() => true)
+    .catch(() => false)
 }
