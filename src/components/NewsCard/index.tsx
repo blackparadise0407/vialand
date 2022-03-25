f2e7a4439a8405268c61d576792a831978323895
blackparadise0407 <blackparadise0407@gmail.com>
2022-03-25 23:30:58 +0700

    Change NewsCard image with default static image

--- src/components/NewsCard/index.tsx
+++ src/components/NewsCard/index.tsx
@@ -1,3 +1,4 @@
+import { second } from 'assets/images'
 import clsx from 'clsx'
 import dayjs from 'dayjs'
 import { Link } from 'react-router-dom'
@@ -10,18 +11,14 @@ type NewsCardProps = {
 export default function NewsCard({ className = '', data }: NewsCardProps) {
   if (!data) return null
 
-  const { subject, images, createdAt } = data
+  const { subject, createdAt } = data
   return (
     <Link
       className={clsx('block hover:shadow', className)}
       to={`/${data.slug}`}
     >
       <div className="flex gap-5 p-2 cursor-pointer h-full">
-        <img
-          className="w-[128px] aspect-video"
-          src={images?.length && images[0].value}
-          alt=""
-        />
+        <img className="w-[128px] aspect-video" src={second} alt="" />
         <div className="overflow-hidden flex flex-col text-base font-light w-full">
           <p className="truncate">{subject}</p>
           <div className="grow"></div>
