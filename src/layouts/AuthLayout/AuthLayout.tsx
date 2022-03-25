f0f3d041a7938e6137a2ecb558a30605148a60e1
blackparadise0407 <blackparadise0407@gmail.com>
2022-03-25 23:32:14 +0700

    Adjust AuthLayout logic

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,12 +1,13 @@
-import { useAuthContext } from 'contexts/AuthContext'
 import { Navigate, Outlet } from 'react-router-dom'
+
+import { useAuthContext } from 'contexts/AuthContext'
 
 export default function AuthLayout() {
   const { isAuth } = useAuthContext()
 
-  // if (!isAuth) {
-  //   return <Navigate to="/dang-nhap" />
-  // }
+  if (!isAuth) {
+    return <Navigate to="/dang-nhap" />
+  }
 
   return <Outlet />
 }
