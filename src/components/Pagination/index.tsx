4d84e1c211ba8be6693a233a49d19d21c6d26ecb
blackparadise0407 <blackparadise0407@gmail.com>
2022-03-25 23:31:09 +0700

    Fix pagination component

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,18 +1,15 @@
-import clsx from 'clsx'
 import { memo } from 'react'
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi'
 
 type PaginationProps = {
-  reachStart?: boolean
-  reachEnd?: boolean
+  hideNavigation?: boolean
   onFirst?: () => void
   onNext?: () => void
   onPrev?: () => void
 }
 
 export default memo(function Pagination({
-  reachStart = false,
-  reachEnd = false,
+  hideNavigation = false,
   onFirst = () => {},
   onNext = () => {},
   onPrev = () => {},
@@ -26,28 +23,24 @@ export default memo(function Pagination({
         <span>Trang đầu</span>
       </button>
       <div className="grow"></div>
-      <button
-        className={clsx(
-          'flex items-center justify-center px-2 py-1 bg-[#111] hover:bg-opacity-70 transition-colors text-white rounded',
-          reachStart && 'pointer-events-none bg-gray-400',
-        )}
-        disabled={reachStart}
-        onClick={onPrev}
-      >
-        <HiChevronDoubleLeft />
-        <span>Trước</span>
-      </button>
-      <button
-        className={clsx(
-          'flex items-center justify-center px-2 py-1 bg-[#111] hover:bg-opacity-70 transition-colors text-white rounded',
-          reachEnd && 'pointer-events-none bg-gray-400',
-        )}
-        disabled={reachEnd}
-        onClick={onNext}
-      >
-        <span>Sau</span>
-        <HiChevronDoubleRight />
-      </button>
+      {!hideNavigation && (
+        <>
+          <button
+            className="flex items-center justify-center px-2 py-1 bg-[#111] hover:bg-opacity-70 transition-colors text-white rounded"
+            onClick={onPrev}
+          >
+            <HiChevronDoubleLeft />
+            <span>Trước</span>
+          </button>
+          <button
+            className="flex items-center justify-center px-2 py-1 bg-[#111] hover:bg-opacity-70 transition-colors text-white rounded"
+            onClick={onNext}
+          >
+            <span>Sau</span>
+            <HiChevronDoubleRight />
+          </button>
+        </>
+      )}
     </div>
   )
 })
