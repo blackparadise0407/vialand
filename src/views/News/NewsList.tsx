a48a39e5de37771cb4e69c02595056d534a07f88
blackparadise0407 <blackparadise0407@gmail.com>
2022-04-08 15:31:42 +0700

    Reimplement NewsList

--- src/views/News/NewsList.tsx
+++ src/views/News/NewsList.tsx
@@ -1,144 +1,145 @@
 import { useCallback, useEffect, useState } from 'react'
-import {
-  collection,
-  DocumentData,
-  endBefore,
-  getDocs,
-  limit,
-  orderBy,
-  query,
-  QueryConstraint,
-  QueryDocumentSnapshot,
-  startAfter,
-  where,
-} from 'firebase/firestore'
 import { useLocation } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import qs from 'query-string'
 
+import { getNews } from 'apis'
 import { Filter, NewsCard, Pagination, Result } from 'components'
 import { PAGE_LIMIT } from 'constants/common'
 import { RETRY_ERROR } from 'constants/message'
 import { EAction } from 'enums'
-import { db } from 'libs/firebase'
+
+const _writeToUrl = (originPath: string, obj: any) => {
+  window.history.replaceState(null, null, originPath + '?' + qs.stringify(obj))
+}
+
+const getInitialFilterFromUrl = () => {
+  const filter = {}
+  const query = qs.parse(window.location.search)
+  Object.entries(query).forEach(([key, val]) => {
+    switch (key) {
+      case 'ward':
+      case 'province':
+      case 'district':
+        ;(filter as any)[key] = !!val ? parseInt(val as any, 10) : undefined
+        break
+      default:
+        break
+    }
+  })
+  return filter as AddressFilter
+}
 
 const initialFilter: AddressFilter = {
-  ward: undefined,
   district: undefined,
+  ward: undefined,
   province: undefined,
 }
 
 export default function NewsList() {
-  const { pathname } = useLocation()
-  const [newsList, setNewsList] = useState<IProperty[]>([])
-  const [fetched, setFetched] = useState(false)
-  const [docPair, setDocPair] = useState<
-    [QueryDocumentSnapshot<DocumentData>, QueryDocumentSnapshot<DocumentData>]
-  >([null, null])
+  const { pathname, search } = useLocation()
   const [loading, setLoading] = useState(false)
-  const [constraint, setConstraint] = useState<QueryConstraint[]>([])
-  const [filter, setFilter] = useState<AddressFilter>(initialFilter)
+  const [state, setState] = useState<{
+    data: IProperty[]
+    filter: AddressFilter
+    page: number
+    limit: number
+    total: number
+    fetched: boolean
+  }>({
+    data: [],
+    filter: getInitialFilterFromUrl(),
+    page: 1,
+    limit: PAGE_LIMIT,
+    total: 0,
+    fetched: false,
+  })
 
   const releaseState = () => {
-    setNewsList([])
-    setDocPair([null, null])
-    setFetched(false)
-    setConstraint([])
-    setFilter(initialFilter)
+    setState({
+      data: [],
+      filter: initialFilter,
+      page: 1,
+      limit: PAGE_LIMIT,
+      total: 0,
+      fetched: false,
+    })
   }
 
-  const fetchData = useCallback(
-    async (...constraints: QueryConstraint[]) => {
+  const handlePageChange = useCallback(
+    (page: number) => {
+      setState((prev) => ({ ...prev, page }))
+      _writeToUrl(pathname, { page, ...state.filter })
+    },
+    [pathname, state.filter],
+  )
+
+  const handleChangeFilter = useCallback(
+    (v: AddressFilter) => {
+      setState((prev) => ({ ...prev, filter: v }))
+      _writeToUrl(pathname, { page: state.page, ...v })
+    },
+    [pathname, state.page],
+  )
+
+  const handleClearFilter = useCallback(() => {
+    setState((prev) => ({ ...prev, filter: initialFilter }))
+    _writeToUrl(pathname, { page: state.page })
+  }, [pathname, state.page])
+
+  useEffect(() => {
+    const filter = {}
+    const q: any = {}
+    const query = qs.parse(search)
+    Object.entries(query).forEach(([key, val]) => {
+      switch (key) {
+        case 'ward':
+        case 'province':
+        case 'district':
+          ;(filter as any)[key] = parseInt(val as any, 10)
+          break
+        case 'page':
+          q['page'] = val
+          break
+        case 'limit':
+          q['limit'] = val
+          break
+        default:
+          break
+      }
+    })
+    setState((prev) => ({ ...prev, filter: filter as AddressFilter }))
+  }, [pathname, search])
+
+  useEffect(() => {
+    setState((prev) => ({ ...prev, fetched: false }))
+  }, [state.filter, state.page])
+
+  useEffect(() => {
+    async function eff() {
       setLoading(true)
+      const { limit, page, filter } = state
       const action = pathname.startsWith('/mua-ban')
         ? EAction.trade
         : EAction.rent
-
-      try {
-        const docsRef = collection(db, 'properties')
-        const q = query(
-          docsRef,
-          where('action', '==', action),
-          where('published', '==', true),
-          orderBy('createdAt', 'desc'),
-          limit(PAGE_LIMIT),
-          ...constraints,
-          ...constraint,
-        )
-
-        const snapshot = await getDocs(q)
-        snapshot.docs?.length &&
-          setDocPair([
-            snapshot.docs[0],
-            snapshot.docs[snapshot.docs.length - 1],
-          ])
-
-        const newsList: IProperty[] = []
-        snapshot.forEach((doc) => {
-          newsList.push({
-            id: doc.id,
-            ...doc.data(),
-          } as any)
-        })
-        setNewsList(newsList)
-      } catch (e) {
-        console.log(e)
-        toast.error(RETRY_ERROR)
-      } finally {
-        setLoading(false)
-        setFetched(true)
-      }
-    },
-    [pathname, constraint],
-  )
-
-  const handleFilter = useCallback((v: AddressFilter) => {
-    setFilter(v)
-    if (Object.entries(v).every(([, v]) => !v)) setConstraint([])
-    if (v.ward) {
-      setConstraint([where('ward', '==', v.ward)])
-      return
-    }
-
-    if (v.district) {
-      setConstraint([where('district', '==', v.district)])
-      return
-    }
-
-    if (v.province) {
-      setConstraint([where('province', '==', v.province)])
-      return
-    }
-  }, [])
-
-  const handleClearFilter = useCallback(() => {
-    if (Object.entries(filter).some(([, v]) => !!v)) {
-      setFilter(initialFilter)
-      setConstraint([])
-    }
-  }, [filter])
-
-  const handleChangePage = useCallback(
-    async (next: boolean = false) => {
-      const [first, last] = docPair
-      let constraint: QueryConstraint
-      if (next) {
-        if (last) constraint = startAfter(last)
+      const { data, status } = await getNews({
+        ...filter,
+        limit,
+        page,
+        published: true,
+        action,
+      })
+      if (status === 200) {
+        const { news, total } = data
+        setState((prev) => ({ ...prev, data: news, total, fetched: true }))
       } else {
-        if (first) constraint = endBefore(first)
+        toast.error(RETRY_ERROR)
+        setState((prev) => ({ ...prev, fetched: true }))
       }
-      constraint && fetchData(constraint)
-    },
-    [docPair, fetchData],
-  )
-
-  useEffect(() => {
-    setFetched(false)
-  }, [constraint, pathname])
-
-  // First run
-  useEffect(() => {
-    !fetched && fetchData()
-  }, [fetched])
+      setLoading(false)
+    }
+    !state.fetched && eff()
+  }, [state.fetched, pathname])
 
   useEffect(() => {
     return () => {
@@ -146,26 +147,30 @@ export default function NewsList() {
     }
   }, [pathname])
 
-  if (loading) return <Result title="Đang lấy thông tin..." />
-
   return (
     <div className="p-5 flex items-center flex-col">
       <Filter
-        value={filter}
-        onFilter={handleFilter}
+        value={state.filter}
+        onFilter={handleChangeFilter}
         onClear={handleClearFilter}
       />
-      {!newsList.length && <Result title="Không có thông tin" />}
+      {!loading && !state.data.length && <Result title="Không có thông tin" />}
       <div className="max-w-[1000px] w-full">
-        {newsList.map((x) => (
-          <NewsCard className="min-h-[150px]" key={x.id} data={x} />
-        ))}
+        {loading ? (
+          <Result title="Đang lấy thông tin..." />
+        ) : (
+          <>
+            {state.data.map((x) => (
+              <NewsCard className="min-h-[150px]" key={x.id} data={x} />
+            ))}
+          </>
+        )}
       </div>
       <Pagination
-        hideNavigation={!newsList.length}
-        onFirst={() => fetchData()}
-        onPrev={() => handleChangePage()}
-        onNext={() => handleChangePage(true)}
+        total={state.total}
+        page={state.page}
+        limit={state.limit}
+        onPageChange={handlePageChange}
       />
     </div>
   )
