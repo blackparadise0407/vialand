47d00dfe3db20e1c277361bf6475d0d81b1f9428
blackparadise0407 <blackparadise0407@gmail.com>
2022-04-08 13:25:31 +0700

    Reimplement Pagination component

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,42 +1,93 @@
-import { memo } from 'react'
+import { memo, useCallback, useMemo } from 'react'
+import clsx from 'clsx'
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi'
 
+import { DOTS, usePagination } from './usePagination'
+
 type PaginationProps = {
-  hideNavigation?: boolean
-  onFirst?: () => void
-  onNext?: () => void
-  onPrev?: () => void
+  total?: number
+  page?: number
+  limit?: number
+  onPageChange?: (page: number) => void
 }
 
 export default memo(function Pagination({
-  hideNavigation = false,
-  onFirst = () => {},
-  onNext = () => {},
-  onPrev = () => {},
+  total = 0,
+  page = 1,
+  limit = 5,
+  onPageChange = () => {},
 }: PaginationProps) {
+  const paginationRange = usePagination({
+    currentPage: page,
+    totalCount: total,
+    siblingCount: 1,
+    pageSize: limit,
+  })
+
+  const handleChangeNextPage = () => {
+    onPageChange(page + 1)
+  }
+
+  const handleChangePrevPage = () => {
+    onPageChange(page - 1)
+  }
+
+  const handleChangePage = useCallback(
+    (_page: number) => {
+      if (_page === page) return
+      onPageChange(_page)
+    },
+    [page, onPageChange],
+  )
+
+  const totalPage = useMemo(() => Math.floor(total / limit), [total, limit])
+
   return (
     <div className="flex my-3 space-x-2">
-      <button
-        className="flex items-center justify-center px-2 py-1 bg-[#111] hover:bg-opacity-70 transition-colors text-white rounded"
-        onClick={onFirst}
-      >
-        <span>Trang đầu</span>
-      </button>
-      <div className="grow"></div>
-      {!hideNavigation && (
+      {totalPage > 0 && (
         <>
           <button
-            className="flex items-center justify-center px-2 py-1 bg-[#111] hover:bg-opacity-70 transition-colors text-white rounded"
-            onClick={onPrev}
+            className={clsx(
+              'flex items-center justify-center w-[30px] h-[30px] px-2 py-1 bg-[#111] hover:bg-opacity-70 border border-transparent transition-colors text-white rounded',
+              page === 1 && 'pointer-events-none bg-[#aeaeae]',
+            )}
+            onClick={handleChangePrevPage}
           >
             <HiChevronDoubleLeft />
-            <span>Trước</span>
           </button>
+
+          {paginationRange?.map((x, idx) => {
+            if (x === DOTS)
+              return (
+                <div
+                  key={idx + 'dots'}
+                  className="w-[30px] h-[30px] text-center flex items-end justify-center"
+                >
+                  ...
+                </div>
+              )
+            return (
+              <button
+                key={x}
+                className={clsx(
+                  'flex items-center justify-center w-[30px] h-[30px] px-2 py-1 hover:bg-opacity-70 border transition-colors rounded',
+                  page === x
+                    ? 'text-[#111] bg-white border-[#111]'
+                    : 'bg-[#111] text-white  border-transparent',
+                )}
+                onClick={() => handleChangePage(x as number)}
+              >
+                <span>{x}</span>
+              </button>
+            )
+          })}
           <button
-            className="flex items-center justify-center px-2 py-1 bg-[#111] hover:bg-opacity-70 transition-colors text-white rounded"
-            onClick={onNext}
+            className={clsx(
+              'flex items-center justify-center w-[30px] h-[30px] px-2 py-1 bg-[#111] hover:bg-opacity-70 border border-transparent transition-colors text-white rounded',
+              page >= totalPage && 'pointer-events-none bg-[#aeaeae]',
+            )}
+            onClick={handleChangeNextPage}
           >
-            <span>Sau</span>
             <HiChevronDoubleRight />
           </button>
         </>
