108e3641fed843e0225a20cb503933edfa11d0a4
blackparadise0407 <blackparadise0407@gmail.com>
2022-04-12 22:29:54 +0700

    Add icon link

--- src/layouts/MainLayout/Footer.tsx
+++ src/layouts/MainLayout/Footer.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react'
 import { useTranslation } from 'react-i18next'
+import { FaFacebookF, FaSkype, FaYoutube } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 type FooterSectionProps = {
@@ -31,6 +32,32 @@ export default function Footer() {
       <footer className="p-[40px] md:py-[50px] md:px-[100px] flex justify-between flex-wrap space-x-[30px]">
         <FooterSection className="md:w-[40%]" title="Vialand">
           <p className="text-[#999]">{t('footer.description')}</p>
+          <div className="flex gap-5 mt-5">
+            <a
+              className="w-[40px] h-[40px] grid place-items-center rounded-full text-xl text-white bg-[#EE0007]"
+              href="https://www.youtube.com/channel/UC-RgeGg8IXuNBl9gqEeqJoQ"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaYoutube />
+            </a>
+            <a
+              className="w-[40px] h-[40px] grid place-items-center rounded-full text-xl text-white bg-[#10BEF2]"
+              href="/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaSkype />
+            </a>
+            <a
+              className="w-[40px] h-[40px] grid place-items-center rounded-full text-xl text-white bg-[#1771E6]"
+              href="https://www.facebook.com/vialand.co"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFacebookF />
+            </a>
+          </div>
         </FooterSection>
         <FooterSection className="hidden md:block" title="Quick Links">
           <ul className="flex flex-col space-y-4">
@@ -93,7 +120,7 @@ export default function Footer() {
       </footer>
       <div className="py-[10px] px-[100px] bg-[#161616]">
         <p className="text-[#999] text-center">
-          Copyright © 2020 ViaLand. All Rights Reserved.
+          Nhận Thiết Kế Web App Mobile Android & iOS (0966885501)
         </p>
       </div>
     </div>
